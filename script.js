18b780ee13f02dc41352bdf79471fbfb97eead94
mubzie <85705074+mubzie@users.noreply.github.com>
2023-09-08 11:50:37 +0100

    updated UTC once again

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const getTime = document.getElementById("current-time");
 const getUserName = document.getElementById("user-name");
 const getTrack = document.getElementById("track");
 
-// set day
 const today = new Date().getDay();
 const daysOfWeek = [
   "sunday",
@@ -14,18 +13,15 @@ const daysOfWeek = [
   "Friday",
   "Saturday",
 ];
-getDay.textContent = `Current Day of the Week: ${daysOfWeek[today]}`;
+getDay.textContent = `Day: ${daysOfWeek[today]}`;
 
-// set time
 const getCurrentTime = () => {
   const getDate = new Date();
-  const getUTC = getDate.getUTCMilliseconds(getDate);
-  getTime.textContent = `Current UTC Time: ${getUTC} ms`;
+  const getUTC = getDate.toUTCString(getDate);
+  getTime.textContent = `Time: ${getUTC}`;
 };
 setInterval(getCurrentTime, 1000);
 
-// set userName
 getUserName.textContent = "Slack Name: Mubzie";
 
-//set track
 getTrack.textContent = "Track: Frontend";
